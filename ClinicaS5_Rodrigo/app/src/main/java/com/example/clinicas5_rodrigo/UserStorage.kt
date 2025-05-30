951d49e3231419e7fc83b65596d4ee293a64cd80
RodrigoM13 <127703172+RodrigoM13@users.noreply.github.com>
2025-05-30 00:03:28 -0500

    Update UserStorage.kt

--- ClinicaS5_Rodrigo/app/src/main/java/com/example/clinicas5_rodrigo/UserStorage.kt
+++ ClinicaS5_Rodrigo/app/src/main/java/com/example/clinicas5_rodrigo/UserStorage.kt
@@ -5,15 +5,18 @@ import com.example.clinicas5_rodrigo.User
 object UserStorage {
 
     private val users = mutableListOf<User>()
-
+    
+    // Método para agregar un nuevo usuario a la lista
     fun addUser(user: User) {
         users.add(user)
     }
-
+    
+    // Método para verificar si un usuario con el nombre dado ya existe en la lista
     fun userExists(username: String): Boolean {
         return users.any { it.username == username }
     }
-
+    
+    // Método para validar las credenciales de usuario (nombre y contraseña)
     fun validateUser(username: String, password: String): Boolean {
         return users.any { it.username == username && it.password == password }
     }
